8ae4525ab42fd4925ce248374bc007a5cbfd8d8d
Cesar <cesarvargasescorcia@gmail.com>
2020-10-04 17:22:43 -0500

    NG: agregando boton que habilita o desasbilita - directiva ngIf

--- angular/clientes-app/src/app/directiva/directiva.component.ts
+++ angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent  {
 
   listaCurso: String[]= ['Typescript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+
+  habilitar: boolean = true;
   constructor() { }
 
- 
+ setHabilitar():void {
+   this.habilitar = (this.habilitar == true)? false : true;
+ }
+
 }
