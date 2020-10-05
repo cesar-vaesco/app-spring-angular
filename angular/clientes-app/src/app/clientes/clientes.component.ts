472de7c4138ab8cfe8e46d2adb79b8a0abe5294c
Cesar <cesarvargasescorcia@gmail.com>
2020-10-05 10:48:13 -0500

    NG: Creando archivo clientes.jason.ts y cambiando la lista de clientes al mismo

--- angular/clientes-app/src/app/clientes/clientes.component.ts
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,91 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
 })
 export class ClientesComponent implements OnInit {
-  clientes: Cliente[] = [
-    {
-      id: 1,
-      nombre: 'César',
-      apellido: 'Vargas',
-      email: 'ces@var.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 2,
-      nombre: 'Omar',
-      apellido: 'Lop',
-      email: 'om@vlop.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 3,
-      nombre: 'Iván',
-      apellido: 'San',
-      email: 'ivn@lop.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 4,
-      nombre: 'Rodrí',
-      apellido: 'Per',
-      email: 'rod@per.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 5,
-      nombre: 'Leo',
-      apellido: 'Mar',
-      email: 'leo@mar.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 6,
-      nombre: 'Arthur',
-      apellido: 'Varela',
-      email: 'art@var.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 7,
-      nombre: 'Raúl',
-      apellido: 'Sol',
-      email: 'rul@sol.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 8,
-      nombre: 'Omar',
-      apellido: 'Lop',
-      email: 'om@vlop.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 9,
-      nombre: 'Iván',
-      apellido: 'San',
-      email: 'ivn@lop.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 10,
-      nombre: 'Rodrí',
-      apellido: 'Per',
-      email: 'rod@per.com',
-      createAt: '2017-12-11',
-    },
-    {
-      id: 11,
-      nombre: 'Leo',
-      apellido: 'Mar',
-      email: 'leo@mar.com',
-      createAt: '2017-12-11',
-    },
-  ];
+  clientes: Cliente[] ;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.clientes = CLIENTES;
+  }
 }
---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,80 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] =  [
+    {
+      id: 1,
+      nombre: 'César',
+      apellido: 'Vargas',
+      email: 'ces@var.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 2,
+      nombre: 'Omar',
+      apellido: 'Lop',
+      email: 'om@vlop.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 3,
+      nombre: 'Iván',
+      apellido: 'San',
+      email: 'ivn@lop.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 4,
+      nombre: 'Rodrí',
+      apellido: 'Per',
+      email: 'rod@per.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 5,
+      nombre: 'Leo',
+      apellido: 'Mar',
+      email: 'leo@mar.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 6,
+      nombre: 'Arthur',
+      apellido: 'Varela',
+      email: 'art@var.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 7,
+      nombre: 'Raúl',
+      apellido: 'Sol',
+      email: 'rul@sol.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 8,
+      nombre: 'Omar',
+      apellido: 'Lop',
+      email: 'om@vlop.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 9,
+      nombre: 'Iván',
+      apellido: 'San',
+      email: 'ivn@lop.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 10,
+      nombre: 'Rodrí',
+      apellido: 'Per',
+      email: 'rod@per.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 11,
+      nombre: 'Leo',
+      apellido: 'Mar',
+      email: 'leo@mar.com',
+      createAt: '2017-12-11',
+    }];
