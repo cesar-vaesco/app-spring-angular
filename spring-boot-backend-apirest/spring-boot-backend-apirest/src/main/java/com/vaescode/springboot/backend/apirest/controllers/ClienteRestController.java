fc76333d86da301472345d7d55207b6f844f517c
Cesar <cesarvargasescorcia@gmail.com>
2020-10-06 17:27:33 -0500

    Spring-NG: uso de cors para compartir recursos API REST - ANgular HttpClient

--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.vaescode.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.vaescode.springboot.backend.apirest.models.entity.Cliente;
 
 import com.vaescode.springboot.backend.apirest.models.service.IClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('César', 'Vargas', 'ces@var.com', '2010-01-01');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Vane', 'Vargas', 'vane@var.com', '2018-05-25');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Vargas', 'glo@var.com', '2013-08-29');
