2be7cc3c284829d5cbcc5ea20a2d82c93405986b
Cesar <cesarvargasescorcia@gmail.com>
2020-10-06 16:52:33 -0500

    Spring: Creando controlador @RestController y EndPoint para listar clientes

---
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,25 @@
+package com.vaescode.springboot.backend.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vaescode.springboot.backend.apirest.models.entity.Cliente;
+
+import com.vaescode.springboot.backend.apirest.models.service.IClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/clientes")
+	public List<Cliente> index(){
+		return clienteService.findAll();
+	}
+}
