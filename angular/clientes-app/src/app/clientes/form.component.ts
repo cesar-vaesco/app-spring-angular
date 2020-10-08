5e1ea8a29f68286cdcbc9fa8b2ade832809454c9
Cesar <cesarvargasescorcia@gmail.com>
2020-10-07 20:39:07 -0500

    NG: Escribiendo el update(actualizar) en el cliente.service.ts y en form.component.ts

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -6,36 +6,53 @@ import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html'
+  templateUrl: './form.component.html',
 })
 export class FormComponent implements OnInit {
+  public cliente: Cliente = new Cliente();
+  public titulo: string = 'Crear cliente';
 
-public cliente: Cliente = new Cliente()
-public titulo:string = "Crear cliente"
-
-  constructor(private clienteService:ClienteService,
-              private router: Router,
-              private actitivateRoute: ActivatedRoute ) { }
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private actitivateRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.cargarCliente()
+    this.cargarCliente();
   }
 
-  cargarCliente(): void{
-    this.actitivateRoute.params.subscribe(params => {
-      let id = params['id']
-      if(id){
-        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+  cargarCliente(): void {
+    this.actitivateRoute.params.subscribe((params) => {
+      let id = params['id'];
+      if (id) {
+        this.clienteService
+          .getCliente(id)
+          .subscribe((cliente) => (this.cliente = cliente));
       }
-    })
+    });
   }
 
-  public create(): void {
-   this.clienteService.create(this.cliente)
-   .subscribe(cliente => {
-       this.router.navigate(['/clientes'])
-       swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
-     } 
-   )
+  create(): void {
+    this.clienteService.create(this.cliente).subscribe((cliente) => {
+      this.router.navigate(['/clientes']);
+      swal.fire(
+        'Nuevo cliente',
+        `Cliente ${cliente.nombre} creado con éxito!`,
+        'success'
+      );
+    });
+  }
+
+  update():void{
+    this.clienteService.update(this.cliente)
+    .subscribe(cliente =>{
+      this.router.navigate(['/clientes'])
+      swal.fire(
+        'Cliente Actualizado',
+        `Cliente ${cliente.nombre} actualizado con éxito!`,
+        'success'
+      );
+    })
   }
 }
--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -1,3 +1,12 @@
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('César', 'Vargas', 'ces@var.com', '2010-01-01');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Vane', 'Vargas', 'vane@var.com', '2018-05-25');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Vargas', 'glo@var.com', '2013-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Verónica', 'Jiménez', 'ver@jim.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Alejandro', 'Cardenas', 'ale@car.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mario', 'Marín', 'mar@mar.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rogelio', 'Chavéz', 'ro@cha.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Raúl', 'Sanchéz', 'rul@san.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Cielo', 'Azul', 'ciel@azul.com', '2020-10-07');		
+	
+
+	
