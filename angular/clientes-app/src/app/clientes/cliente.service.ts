72ff2ee81e672c015667c6641f20a39cf63b3208
Cesar <cesarvargasescorcia@gmail.com>
2020-10-09 15:07:53 -0500

    NG: Manejo de errores en el front-end obtenido por id

--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { of, Observable }from 'rxjs';
+import { of, Observable, throwError }from 'rxjs';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 
@@ -16,7 +18,8 @@ export class ClienteService {
 
 private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
-  constructor( private http: HttpClient) { }
+/* Constructor */
+  constructor( private http: HttpClient, private router:Router ) { }
 
   getClientes() : Observable<Cliente[]>{
    //return of(CLIENTES);
@@ -26,11 +29,18 @@ private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
    );
   }
   create(cliente: Cliente): Observable<Cliente>{
-    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
   }
 
   getCliente(id): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError( e => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+            swal.fire('Error al editar', e.error.mensaje,'error');
+            return throwError(e);
+      })
+    );
   }
 
   update(cliente: Cliente): Observable<Cliente>{
