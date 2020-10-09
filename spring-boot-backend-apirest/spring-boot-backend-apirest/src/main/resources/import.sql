316fe29c0610147dc7e56aaf8732a70717eba7cb
Cesar <cesarvargasescorcia@gmail.com>
2020-10-08 23:19:43 -0500

    NG: Mensaje en la vista cuando no se tengan clientes que mostrar

--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/resources/import.sql
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('César', 'Vargas', 'ces@var.com', '2010-01-01');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Vane', 'Vargas', 'vane@var.com', '2018-05-25');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Vargas', 'glo@var.com', '2013-08-29');
@@ -15,7 +16,6 @@ INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Alejandro', '
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mario', 'Marín', 'mar@mar.com', '2020-10-07');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rogelio', 'Chavéz', 'ro@cha.com', '2020-10-07');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Raúl', 'Sanchéz', 'rul@san.com', '2020-10-07');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Cielo', 'Azul', 'ciel@azul.com', '2020-10-07');		
-		
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Cielo', 'Azul', 'ciel@azul.com', '2020-10-07');	
 
 	
