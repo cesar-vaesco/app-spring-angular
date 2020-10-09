d19009702e58ed6b1e6f1306b1a56b23c7a41b64
Cesar <cesarvargasescorcia@gmail.com>
2020-10-08 23:53:52 -0500

    Spring: Manejo de error en el backend en método handler update(actualizar)

--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -65,7 +65,6 @@ public class ClienteRestController {
 		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
 
-
 	/**/
 	@PostMapping("/clientes")
 	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
@@ -88,18 +87,39 @@ public class ClienteRestController {
 	}
 
 	@PutMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
-		
+	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+
 		Cliente clienteActual = clienteService.findById(id);
-		
-		clienteActual.setNombre(cliente.getNombre());
-		clienteActual.setApellido(cliente.getApellido());
-		clienteActual.setEmail(cliente.getEmail());
-		
-		return clienteService.save(clienteActual);
+		Cliente clienteUpdate = null;
+
+		Map<String, Object> response = new HashMap<>();
+
+		if (clienteActual == null) {
+
+			response.put("mensaje", "Error: no se puede editar, el cliente ID: "
+					.concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+
+		try {
+			clienteActual.setNombre(cliente.getNombre());
+			clienteActual.setApellido(cliente.getApellido());
+			clienteActual.setEmail(cliente.getEmail());
+			clienteActual.setCreateAt(cliente.getCreateAt());
+
+			clienteUpdate = clienteService.save(clienteActual);
+		} catch (DataAccessException e) {
+			log.info("Log: error al crear el registro");
+			response.put("mensaje", "Error al actualizar en la base de datos !");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		response.put("mensaje", "El cliente ha sido actualizado con éxito!!");
+		response.put("cliente", clienteUpdate);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("/clientes/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
