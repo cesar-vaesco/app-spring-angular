dbae8aa91e03d6df83c49437eb4351f69e72c04b
Cesar <cesarvargasescorcia@gmail.com>
2020-10-11 23:12:36 -0500

    Spring: Implementando anotaciones @Valid en métodos handler create y update del controller

--- spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/src/main/java/com/vaescode/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,9 +1,12 @@
 package com.vaescode.springboot.backend.apirest.controllers;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +24,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vaescode.springboot.backend.apirest.models.entity.Cliente;
@@ -65,12 +69,32 @@ public class ClienteRestController {
 		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
 
-	/**/
+	/*
+	 * @Valid valida las anotaciones de validación marcadas en los atributos de el
+	 * Objeto Cliente.Java
+	 */
 	@PostMapping("/clientes")
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 
 		Cliente clienteNew = null;
 		Map<String, Object> response = new HashMap<>();
+
+		if (result.hasErrors()) {
+
+			/*
+			 * List<String> errors = new ArrayList<>(); for(FieldError err:
+			 * result.getFieldErrors()) { errors.add("El campo '"+err.getField()+"' " +
+			 * err.getDefaultMessage()); }
+			 */
+
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+
 		try {
 			clienteNew = clienteService.save(cliente);
 		} catch (DataAccessException e) {
@@ -87,12 +111,28 @@ public class ClienteRestController {
 	}
 
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
 
 		Cliente clienteActual = clienteService.findById(id);
 		Cliente clienteUpdate = null;
 
 		Map<String, Object> response = new HashMap<>();
+
+		if (result.hasErrors()) {
+
+			/*
+			 * List<String> errors = new ArrayList<>(); for(FieldError err:
+			 * result.getFieldErrors()) { errors.add("El campo '"+err.getField()+"' " +
+			 * err.getDefaultMessage()); }
+			 */
+
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 
 		if (clienteActual == null) {
 
