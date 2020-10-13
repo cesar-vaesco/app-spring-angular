10ad61c46472dbfa016150407637c1604b352829
Cesar <cesarvargasescorcia@gmail.com>
2020-10-12 19:34:04 -0500

    NG: Agregando la ruta page del paginador y el Observable ActivatedRoute

--- angular/clientes-app/src/app/clientes/clientes.component.ts
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@ import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import swal from 'sweetalert2';
 import { tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
@@ -11,23 +12,37 @@ import { tap } from 'rxjs/operators';
 export class ClientesComponent implements OnInit {
   clientes: Cliente[];
 
-  constructor(private clienteService: ClienteService) {}
+  constructor(
+    private clienteService: ClienteService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
-    let page = 0;
-    this.clienteService.getClientes(page).pipe(
-      tap(response =>{
-       
-        console.log('ClientesComponent: tap 3');
-        (response.content as Cliente[]).forEach(cliente =>{
-          console.log(cliente.nombre);
-      });
-    })
-    ).subscribe(response => this.clientes = response.content as Cliente[] );
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let page: number = +params.get('page');
+      if (!page) {
+        page = 0;
+      }
+
+      this.clienteService
+        .getClientes(page)
+        .pipe(
+          tap((response) => {
+            console.log('ClientesComponent: tap 3');
+            (response.content as Cliente[]).forEach((cliente) => {
+              console.log(cliente.nombre);
+            });
+          })
+        )
+        .subscribe(
+          (response) => (this.clientes = response.content as Cliente[])
+        );
+    });
   }
 
   delete(cliente: Cliente): void {
-    swal.fire({
+    swal
+      .fire({
         title: 'Está seguro?',
         text: `¿Seguro que desea eliminar al cliente  ${cliente.nombre} ${cliente.apellido}`,
         icon: 'warning',
@@ -40,17 +55,17 @@ export class ClientesComponent implements OnInit {
         cancelButtonClass: 'btn btn-danger',
         buttonsStyling: false,
         reverseButtons: true,
-      } as any) .then((result) => {
+      } as any)
+      .then((result) => {
         if (result.isConfirmed) {
-          this.clienteService.delete(cliente.id).subscribe(
-            response => {
-              this.clientes = this.clientes.filter(cli => cli !== cliente)
-              swal.fire('Cliente eliminado!', 
-                        `Cliente ${cliente.nombre} eliminado con éxito`,
-                          'success');
-            }
-          )
-          
+          this.clienteService.delete(cliente.id).subscribe((response) => {
+            this.clientes = this.clientes.filter((cli) => cli !== cliente);
+            swal.fire(
+              'Cliente eliminado!',
+              `Cliente ${cliente.nombre} eliminado con éxito`,
+              'success'
+            );
+          });
         }
       });
   }
