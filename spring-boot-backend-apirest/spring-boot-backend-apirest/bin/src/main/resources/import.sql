673c47ded5822f7a0773ad942b0d45010d9ea8d7
Cesar <cesarvargasescorcia@gmail.com>
2020-10-12 20:50:43 -0500

    NG-SPRING: Clonando el poryecto

---
+++ spring-boot-backend-apirest/spring-boot-backend-apirest/bin/src/main/resources/import.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('César', 'Vargas', 'ces@vaescode.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Vane', 'Vargas', 'vane@vaescode.com', '2018-05-25');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Aurelio', 'Vargas', 'yeyo@vaescode.com', '2020-10-07');	
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Vargas', 'glo@vaescode.com', '2013-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Verónica', 'Jiménez', 'ver@vaescode.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Alejandro', 'Cardenas', 'ale@vaescode.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mario', 'Marín', 'mar@vaescode.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rogelio', 'Chavéz', 'ro@vaescode.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Ramona', 'Higareda', 'higa@vaescode.com', '2020-10-07');	
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Raúl', 'Sanchéz', 'rul@vaescode.com', '2020-10-07');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Cielo', 'Azul', 'ciel@vaescode.com', '2020-10-07');		
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Josue', 'Ramiréz', 'jos@vaescode.com', '2020-10-07');	
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Ramón', 'Quezada', 'queza@vaescode.com', '2020-10-07');	
+
+	
