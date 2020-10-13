f26462e35a725797a6ba0f55103b7b2904ab56e2
Cesar <cesarvargasescorcia@gmail.com>
2020-10-12 23:17:23 -0500

    NG: Inyectando paginador usando decorador @Input  en el componente Paginador

--- angular/clientes-app/src/app/clientes/clientes.component.ts
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -10,7 +10,9 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './clientes.component.html',
 })
 export class ClientesComponent implements OnInit {
+
   clientes: Cliente[];
+  paginador: any;
 
   constructor(
     private clienteService: ClienteService,
@@ -34,9 +36,10 @@ export class ClientesComponent implements OnInit {
             });
           })
         )
-        .subscribe(
-          (response) => (this.clientes = response.content as Cliente[])
-        );
+        .subscribe((response) => {
+          this.clientes = response.content as Cliente[];
+          this.paginador = response;
+        });
     });
   }
 
